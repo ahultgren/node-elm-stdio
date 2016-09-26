89f35d1443fc9a95ef9c1427ae6e47107f9c5946
Andreas Hultgren <me@andreashultgren.se>
2016-09-26 21:11:39 +0200

    Add --name

--- stdio/index.js
+++ stdio/index.js
@@ -4,9 +4,9 @@ var Node = require("./node");
 
 Node.globalDocument();
 
-module.exports = function (Elm, data, callback) {
+module.exports = function (Elm, data, name, callback) {
   var rootNode = new Node();
-  Elm.Generate.embed(rootNode, data);
+  Elm[name].embed(rootNode, data);
   setTimeout(() => {
     callback(null, rootNode.stringify());
   }, 0);
